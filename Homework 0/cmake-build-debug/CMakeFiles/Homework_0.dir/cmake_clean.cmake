3f7b06d00e27cf02514fb8be97900be4dd11ac1d
Kyle Jacobson <54565291+jacokyle@users.noreply.github.com>
2020-04-15 00:21:54 -0400

    Hello World

---
+++ Homework 0/cmake-build-debug/CMakeFiles/Homework_0.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Homework_0.dir/main.cpp.obj"
+  "Homework_0.pdb"
+  "Homework_0.exe"
+  "Homework_0.exe.manifest"
+  "libHomework_0.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Homework_0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
